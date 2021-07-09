c616ea545935d8b36ee2cedcd5305c69d0b05958
cancerogen-stree <don544@mail.ru>
2021-07-10 00:37:23 +0500

    create_users_table

--- database/migrations/2021_07_09_193514_create_users_table.php
+++ database/migrations/2021_07_09_193514_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id')
+                ->constrained('roles')
+                ->cascadeOnDelete();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique();
             $table->string('password');
+            $table->boolean('is_business')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
