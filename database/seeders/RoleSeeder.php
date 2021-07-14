0a4ec221d071ee88be42e20deb5f958a7dbf1ca0
MargoMargo <lisenok2286@yandex.ru>
2021-07-14 16:22:50 +0300

    SQLSTATE[42S22]: Column not found: 1054 Unknown column 'role' in 'field list'

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -18,7 +18,7 @@ class RoleSeeder extends Seeder
 
         foreach ($roles as $role) {
             DB::table('roles')->insert([
-                'role' => $role,
+                'name' => $role,
             ]);
         }
     }
