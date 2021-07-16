330691b93727decc58b352231a0b65deac6932d3
cancerogen16 <don544@mail.ru>
2021-07-17 00:46:05 +0500

    format MasterController
    # Conflicts:
    #	app/Http/Controllers/Api/MasterController.php

--- app/Http/Controllers/Api/MasterController.php
+++ app/Http/Controllers/Api/MasterController.php
@@ -3,18 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\MasterRequest;
 use App\Models\Master;
 use App\Traits\ApiResponder;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use App\Http\Requests\MasterRequest;
 
 
 class MasterController extends Controller
 {
     use ApiResponder;
 
-    public function index(Request $request): JsonResponse
+    public function index(): JsonResponse
     {
         $data = Master::all();
 
@@ -27,34 +27,51 @@ class MasterController extends Controller
 
         $master->save();
 
-        return $this->handleResponse($master, 201);
+        if ($master) {
+            return $this->handleResponse($master, 201);
+
+        } else {
+            return $this->handleError('Ошибка при добавлении мастера', [], 404);
+        }
+
     }
 
-    public function show(Request $request, $id): JsonResponse
+    public function show($id): JsonResponse
     {
-        $master = Master::findOrFail($id);
+        try {
+            $master = Master::findOrFail($id);
 
-        return  $this->handleResponse($master->toArray());
+            return $this->handleResponse($master->toArray());
+        } catch (ModelNotFoundException $e) {
+            return $this->handleError($e, ['Ошибка при поиске мастера'], 404);
+        }
     }
 
     public function update(MasterRequest $request, int $id): JsonResponse
     {
-        $master = Master::findOrFail($id);
+        try {
+            $master = Master::findOrFail($id);
 
-        $data = $request->validated();
+            $data = $request->validated();
 
-        $master->update($data);
+            $master->update($data);
 
-        return $this->handleResponse($master, 'Updated');
+            return $this->handleResponse($master, 'Updated');
+        } catch (ModelNotFoundException $e) {
+            return $this->handleError($e, ['Ошибка при поиске мастера'], 404);
+        }
     }
 
     public function delete($id): JsonResponse
     {
-        $master = Master::findOrFail($id);
+        try {
+            $master = Master::findOrFail($id);
 
-        $master->delete();
+            $master->delete();
 
-        return $this->handleResponse($master, 'Deleted');
+            return $this->handleResponse($master, 'Deleted');
+        } catch (ModelNotFoundException $e) {
+            return $this->handleError($e, ['Ошибка при удалении мастера'], 404);
+        }
     }
-
 }
