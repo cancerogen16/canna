327881bbc17103d05c96771179157ce1f13ffb8b
cancerogen16 <don544@mail.ru>
2021-07-17 00:08:03 +0500

    Merge pull request #58 from cancerogen16/rimskaya1
    Сделаны: seeder и model для salons

---
+++ app/Models/Salon.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
+
+class Salon extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'slug',
+        'main_photo',
+        'city',
+        'address',
+        'phone',
+        'worktime',
+        'description',
+        'rating',
+    ];
+}
