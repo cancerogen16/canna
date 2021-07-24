829757d637e9dea7b6c9f3004e771f96c7bfc664
Legotin Anton <ant.legotin@yandex.ru>
2021-07-24 22:34:04 +0500

    Исправлены опечатки. Изменено поле 'Телефон' на 'E-mail'

--- resources/js/components/login/index.js
+++ resources/js/components/login/index.js
@@ -1,34 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button} from '@material-ui/core';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
-
-
-
-import firebase from 'firebase/app';
-import "firebase/analytics";
-
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
 import { useDispatch, useSelector } from 'react-redux';
 import { authLogin } from '../../store/auth/actions';
 import { Redirect } from 'react-router-dom';
-import { editProfile } from '../../store/profile/action';
-
-
-
-var firebaseConfig = {
-    apiKey: "AIzaSyBwf9H3J5uI3W_WGpKcM8le2XDbwBOl9tc",
-    authDomain: "test-5b0ea.firebaseapp.com",
-    projectId: "test-5b0ea",
-    storageBucket: "test-5b0ea.appspot.com",
-    messagingSenderId: "964341762176",
-    appId: "1:964341762176:web:15d74d02c7b86236623e65",
-    measurementId: "G-25CTYC0L3B",
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
 
 
 export default function Login (){
@@ -38,37 +13,24 @@ export default function Login (){
     const [submitted, setSubmitted] = useState(false);
 
     const [credentials, setCredentials] = useState({
-        phone: '',
+        email: '',
         password: '',
         remember: false,
         
     });
-
+    useEffect(() => {
+        setTimeout(() => setSubmitted(false), 1000)
+    })
     const handleSubmit = () => {
-        setSubmitted(true, () => {
-            setTimeout(() => setSubmitted(false), 1000);
-        })
-        firebase.auth().signInWithEmailAndPassword(credentials.phone, credentials.password)
-        .then((userCredential) => {
-          
-          var user = userCredential.user;
-          dispatch(authLogin(user.uid));
-          console.log(user);
-          dispatch(editProfile(user.email, user.uid));
-        })
-        .catch((error) => {
-            console.log(error)
-          var errorCode = error.code;
-          var errorMessage = error.message;
-        });
+        setSubmitted(true);
     }
 
     const handlerOnChangeField = (e) => {
         switch (e.target.name){
-            case 'phone':
+            case 'email':
                 setCredentials({
                     ...credentials,
-                    phone: e.target.value
+                    email: e.target.value
                 })
                 break;
             case 'password': 
@@ -100,12 +62,14 @@ export default function Login (){
         >
             <TextValidator
                 className="form_login__item"
-                label="Телефон"
+                label="E-mail"
                 onChange={handlerOnChangeField}
-                name="phone"
-                value={credentials.phone}
-                validators={['required', 'matchRegexp:^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{5,6}$']}
-                errorMessages={['Поле обязательно для заполнения', 'Номер должен быть в фортмате +7(999) 999 99 99']}
+                name="email"
+                value={credentials.email}
+                type="email"
+                //validators={['required', 'matchRegexp:^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{5,6}$']} Валидация по телефону, оставлена на будущие
+                validators={['required', 'isEmail']}
+                errorMessages={['Поле обязательно для заполнения', 'Некорректный e-mail']}
             />
             <br />
             <TextValidator
@@ -127,7 +91,7 @@ export default function Login (){
                 disabled={submitted}
             >
                 {
-                    (submitted && 'Отправленно!')
+                    (submitted && 'Отправлено!')
                     || (!submitted && 'Войти')
                 }
             </Button>
