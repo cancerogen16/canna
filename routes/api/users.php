44dd9c243006bb0f98e3e45a7be28af9e9a6800d
ruslan-kiiamov <xkijam@gmail.com>
2021-07-28 21:47:23 +0300

    users route modified

--- routes/api/users.php
+++ routes/api/users.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
-//Route::group([
-//    'middleware' => 'auth:sanctum',
-//], function () {
+Route::group([
+    'middleware' => 'auth:sanctum',
+], function () {
     Route::get('/', [UserController::class, 'index'])->name('users.index');
     Route::get('/{id}', [UserController::class, 'show'])->name('users.show');
     Route::match(['put', 'patch'], '/{id}', [UserController::class, 'update'])->name('users.update');
     Route::delete('/{id}', [UserController::class, 'delete'])->name('users.delete');
-//});
+});
