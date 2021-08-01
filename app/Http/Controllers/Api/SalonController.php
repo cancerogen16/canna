d7e20dc079e540c9fe4f57a4d58e0fa859ece26a
Legotin Anton <ant.legotin@yandex.ru>
2021-08-01 19:53:22 +0500

    dashboard

--- app/Http/Controllers/Api/SalonController.php
+++ app/Http/Controllers/Api/SalonController.php
@@ -2,12 +2,151 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SalonSearchRequest;
 use App\Models\Salon;
+use App\Traits\ApiResponder;
+use Illuminate\Http\JsonResponse;
+use App\Http\Requests\SalonRequest;
+use Illuminate\Support\Facades\Auth;
+use Throwable;
 
-class SalonController extends ApiController
+class SalonController extends Controller
 {
-    public function __construct(Salon $model)
+    use ApiResponder;
+
+    public function index(): JsonResponse
     {
-        $this->model = $model;
+        $salons = Salon::all();
+
+        return $this->handleResponse([
+            'salons' => $salons
+        ]);
+    }
+
+    public function store(SalonRequest $request): JsonResponse
+    {
+        try {
+            $salon = new Salon($request->validated());
+
+            if (Auth::user()->cannot('create', $salon)) {
+                return $this->handleResponse([
+                    'errors' => ['Нет доступа'],
+                ]);
+            }
+
+            $salon->save();
+
+            return $this->handleResponse([
+                'salon' => $salon
+            ], 201);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
+    }
+
+    public function show(int $id): JsonResponse
+    {
+        try {
+            $salon = Salon::findOrFail($id);
+
+            return $this->handleResponse([
+                'salon' => $salon->toArray()
+            ]);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
+    }
+
+    public function update(SalonRequest $request, int $id): JsonResponse
+    {
+        try {
+            $salon = Salon::findOrFail($id);
+
+            $data = $request->validated();
+
+            if (Auth::user()->cannot('update', [$salon, $data['user_id']])) {
+                return $this->handleResponse([
+                    'errors' => ['Нет доступа'],
+                ]);
+            }
+
+            $salon->update($data);
+
+            return $this->handleResponse([
+                'salon' => $salon
+            ]);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
+    }
+
+    public function delete(int $id): JsonResponse
+    {
+        try {
+            $salon = Salon::findOrFail($id);
+
+            if (Auth::user()->cannot('delete', $salon)) {
+                return $this->handleResponse([
+                    'errors' => ['Нет доступа'],
+                ]);
+            }
+
+            $salon->delete();
+
+            return $this->handleResponse([
+                'salon' => $salon
+            ]);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
+    }
+
+    public function search(SalonSearchRequest $request)
+    {
+        $result = [];
+
+        $salons = Salon::where('city', $request->input('city'))->get();
+
+        foreach ($salons as $salon) {
+            if ($salon->services()->where('category_id', $request->input('category_id'))->get()) {
+                $result[] = $salon;
+            }
+        }
+
+        return $this->handleResponse([
+            'salons' => $result,
+        ]);
+
+    }
+
+    public function masters(int $id): JsonResponse
+    {
+        try {
+            $salon = Salon::findOrFail($id);
+
+            $masters = $salon->masters()->get();
+
+            return $this->handleResponse([
+                'masters' => $masters,
+            ]);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
+    }
+
+    public function services(int $id): JsonResponse
+    {
+        try {
+            $salon = Salon::findOrFail($id);
+
+            $services = $salon->services()->get();
+
+            return $this->handleResponse([
+                'services' => $services,
+            ]);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
     }
 }
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,12 +2,201 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
+use App\Traits\ApiResponder;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Throwable;
+use function PHPUnit\Framework\isNull;
 
-class UserController extends ApiController
+class UserController extends Controller
 {
-    public function __construct(User $model)
+    use ApiResponder;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
     {
-        $this->model = $model;
+        if (Auth::user()->cannot('viewAny', User::class)) {
+            return $this->handleResponse([
+                'errors' => ['Нет доступа'],
+            ]);
+        }
+
+        $users = User::all();
+
+        return $this->handleResponse([
+            'users' => $users,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return JsonResponse
+     */
+    public function show(int $id): JsonResponse
+    {
+        try {
+            $user = User::findOrFail($id);
+
+            if (Auth::user()->cannot('view', $user)) {
+                return $this->handleResponse([
+                    'errors' => ['Нет доступа'],
+                ]);
+            }
+
+            return $this->handleResponse([
+                'user' => $user->toArray(),
+            ]);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  UserRequest  $request
+     * @param  int  $id
+     * @return JsonResponse
+     */
+    public function update(UserRequest $request, int $id): JsonResponse
+    {
+        try {
+            $user = User::findOrFail($id);
+
+            if (Auth::user()->cannot('update', $user)) {
+                return $this->handleResponse([
+                    'errors' => ['Нет доступа'],
+                ]);
+            }
+
+            $data = $request->validated();
+
+            $user->update($data);
+
+            return $this->handleResponse([
+                'user' => $user,
+            ]);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return JsonResponse
+     */
+    public function delete(int $id): JsonResponse
+    {
+        try {
+            $user = User::findOrFail($id);
+
+            if (Auth::user()->cannot('delete', $user)) {
+                return $this->handleResponse([
+                    'errors' => ['Нет доступа'],
+                ]);
+            }
+
+            $user->delete();
+
+            return $this->handleResponse([
+                'user' => $user,
+            ]);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
+    }
+
+    /**
+     * Display the profile of the specified resource.
+     *
+     * @param  int  $id
+     * @return JsonResponse
+     */
+    public function getProfile(int $id): JsonResponse
+    {
+        try {
+            $user = User::findOrFail($id);
+
+            if (Auth::user()->cannot('view', $user)) {
+                return $this->handleResponse([
+                    'errors' => ['Нет доступа'],
+                ]);
+            }
+
+            $profile = $user->profile()->first();
+            if (isNull($profile)) {
+                $profile = collect([]);
+            }
+
+            return $this->handleResponse([
+                'profile' => $profile->toArray(),
+            ]);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
+    }
+
+    /**
+     * Display the salons listing of the specified resource.
+     *
+     * @param  int  $id
+     * @return JsonResponse
+     */
+    public function getSalons(int $id): JsonResponse
+    {
+        try {
+            $user = User::findOrFail($id);
+
+            if (Auth::user()->cannot('view', $user)) {
+                return $this->handleResponse([
+                    'errors' => ['Нет доступа'],
+                ]);
+            }
+
+            $salons = $user->salons()->get();
+
+            return $this->handleResponse([
+                'salons' => $salons,
+            ]);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
+    }
+
+    /**
+     * Display the records listing of the specified resource.
+     *
+     * @param  int  $id
+     * @return JsonResponse
+     */
+    public function getRecords(int $id)
+    {
+        try {
+            $user = User::findOrFail($id);
+
+            if (Auth::user()->cannot('view', $user)) {
+                return $this->handleResponse([
+                    'errors' => ['Нет доступа'],
+                ]);
+            }
+
+            $records = $user->records()->get();
+
+            return $this->handleResponse([
+                'records' => $records,
+            ]);
+        } catch (Throwable $e) {
+            return $this->handleError($e->getCode(), $e->getMessage());
+        }
     }
 }
