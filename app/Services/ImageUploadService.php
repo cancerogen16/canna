8cea1f990b482d8869a03f87d831827e1268436e
cancerogen16 <don544@mail.ru>
2021-08-04 23:43:13 +0500

    upload UploadedFile

--- app/Services/ImageUploadService.php
+++ app/Services/ImageUploadService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 
 use App\Contracts\UploadImageServiceContract;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\ImageManagerStatic as Image;
 use Throwable;
@@ -12,42 +13,37 @@ use Throwable;
 class ImageUploadService implements UploadImageServiceContract
 {
     /**
-     * @param Request $request
-     * @param string $fieldName
+     * @param UploadedFile $file
      * @param int $width
      * @param int $height
      * @return string|null
      */
-    public function upload(Request $request, string $fieldName = 'image', int $width = 0, int $height = 0): ?string
+    public function upload(UploadedFile $file, int $width = 0, int $height = 0): ?string
     {
         try {
-            if ($request->hasFile($fieldName)) {
-                $file = $request->file($fieldName);
+            $originalName = $file->getClientOriginalName();
 
-                $originalName = $file->getClientOriginalName();
+            $ext = $file->getClientOriginalExtension();
 
-                $ext = $file->getClientOriginalExtension();
+            $filename = str_replace('.' . $ext, '', $originalName);
 
-                $filename = str_replace('.' . $ext, '', $originalName);
+            $fileUploaded = $file->move(Storage::path('images') . '/origin/', $originalName);
 
-                $fileUploaded = $file->move(Storage::path('images') . '/origin/', $originalName);
-
-                if ($fileUploaded === false) {
-                    throw new \Exception('Image upload error');
-                }
-
-                if ($width && $height) {
-                    $thumbnail = Image::make(Storage::path('images') . '/origin/' . $originalName);
-
-                    $thumbnail->fit($width, $height);
-
-                    $newName = $filename . '-' . $width . 'x' . $height . '.' . $ext;
-
-                    $thumbnail->save(Storage::path('images') . '/thumbnail/' . $newName);
-                }
-
-                return $originalName;
+            if ($fileUploaded === false) {
+                throw new \Exception('Image upload error');
             }
+
+            if ($width && $height) {
+                $thumbnail = Image::make(Storage::path('images') . '/origin/' . $originalName);
+
+                $thumbnail->fit($width, $height);
+
+                $newName = $filename . '-' . $width . 'x' . $height . '.' . $ext;
+
+                $thumbnail->save(Storage::path('images') . '/thumbnail/' . $newName);
+            }
+
+            return $originalName;
 
             throw new \Exception('Image upload error');
         } catch (Throwable $e) {
