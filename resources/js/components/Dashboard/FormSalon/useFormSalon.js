65f101348b52fd4ceeb94b90c493960841234eb2
cancerogen16 <don544@mail.ru>
2021-08-09 00:27:42 +0500

    salon image

--- resources/js/components/Dashboard/FormSalon/useFormSalon.js
+++ resources/js/components/Dashboard/FormSalon/useFormSalon.js
@@ -1,75 +1,79 @@
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { ValidatorForm } from 'react-material-ui-form-validator';
-import { fetchRegistre } from '../../../store/auth/thunks';
-import { fetchCreateSalon, fetchSalonByUserId } from '../../../store/salon/thunks';
-import { Route, Redirect } from 'react-router-dom'
-import { updateSalonUserFetch } from '../../../store/user/thunks';
+import {useState} from 'react'
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchCreateSalon} from '../../../store/salon/thunks';
 
-
-
-export default function useFormSalon(props){
+export default function useFormSalon(props) {
     const dispatch = useDispatch();
     const [submitted, setSubmitted] = useState(false);
     const [open, setOpen] = useState(true);
     const user = useSelector(state => state.user);
     const [credentials, setCredentials] = useState({
         user_id: user.id,
-        title: '', 
-        city: '', 
-        address: '', 
+        title: '',
+        city: '',
+        address: '',
         phone: '',
-        description: '' 
+        description: '',
+        main_photo: []
     });
 
-    const handleSubmit = (props) => {
-        dispatch(fetchCreateSalon(credentials));
+    const handleSubmit = (e) => {
+        const form = new FormData(e.target.form);
+        form.append('user_id',credentials.user_id);
+        form.append('title', credentials.title)
+        form.append('city', credentials.city)
+        form.append('phone', credentials.phone)
+        form.append('address', credentials.address)
+        form.append('description', credentials.description)
+        form.append('main_photo', credentials.main_photo)
+        console.log(e.target.form)
+
+        dispatch(fetchCreateSalon(form));
         setSubmitted(true);
         setOpen(false);
     }
 
-    
-
     const handlerOnChangeField = (e) => {
-        switch (e.target.name){
+        switch (e.target.name) {
             case 'title':
                 setCredentials({
                     ...credentials,
                     title: e.target.value
                 })
                 break;
-            case 'city': 
+            case 'city':
                 setCredentials({
                     ...credentials,
                     city: e.target.value
                 })
-            break;
-            case 'address': 
+                break;
+            case 'address':
                 setCredentials({
                     ...credentials,
                     address: e.target.value
                 })
-            break;
-            case 'phone': 
+                break;
+            case 'phone':
                 setCredentials({
                     ...credentials,
                     phone: e.target.value
                 })
                 break;
-            case 'description': 
-            setCredentials({
-                ...credentials,
-                description: e.target.value
-            })
-            break;
+            case 'description':
+                setCredentials({
+                    ...credentials,
+                    description: e.target.value
+                })
+                break;
+            case 'main_photo':
+                setCredentials({
+                    ...credentials,
+                    main_photo: e.target.files[0]
+                })
+                break;
         }
     }
-    
-    
-    
-    
-   
-    
+
     return {
         handlerOnChangeField,
         handleSubmit,
--- resources/js/store/salon/thunks.js
+++ resources/js/store/salon/thunks.js
@@ -1,38 +1,33 @@
 import HTTP from '../../utils/HTTP';
-import { updateSalonUser } from '../user/action';
-import { addSalon, clearSalon } from './action';
+import {updateSalonUser} from '../user/action';
+import {addSalon, clearSalon} from './action';
 
-export const fetchCreateSalon = (salons) => (dispatch, getState) => {
-
-    HTTP.post('api/salons', salons)
-    .then(res => {
-        
-        const salon = res.data.salon;
-        dispatch(addSalon(salon));
-        dispatch(updateSalonUser(salon.id));
-
+export const fetchCreateSalon = (form) => (dispatch, getState) => {
+    HTTP.post('api/salons', form, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
     })
-
+        .then(res => {
+            const salon = res.data.salon;
+            dispatch(addSalon(salon));
+            dispatch(updateSalonUser(salon.id));
+        })
 }
 
 export const fetchSalonByUserId = (user_id) => (dispatch, getState) => {
-    
-        HTTP.get(`api/users/${user_id}/salons`)
+    HTTP.get(`api/users/${user_id}/salons`)
         .then(res => dispatch(addSalon(res.data.salons[0])))
-   
 }
 
 export const fetchSalonsOneId = (id) => (dispatch, getState) => {
-
     HTTP.get(`/api/salons/${id}`)
-    .then(res => {
-        if(res.data.code){
-            dispatch(clearSalon())
-        }else{
-            console.log('sad',res)
-            dispatch(addSalon(res.data.salon))
-        }
-        
-    });
-
+        .then(res => {
+            if (res.data.code) {
+                dispatch(clearSalon())
+            } else {
+                console.log('sad', res)
+                dispatch(addSalon(res.data.salon))
+            }
+        });
 }
