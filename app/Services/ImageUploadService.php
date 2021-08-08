94fe175284f50ed2a295205cdb8536a26b558eaa
cancerogen16 <don544@mail.ru>
2021-08-08 11:29:17 +0500

    getImage

--- app/Services/ImageUploadService.php
+++ app/Services/ImageUploadService.php
@@ -40,45 +40,81 @@ class ImageUploadService implements UploadImageServiceContract
      * @param int $height
      * @return string
      */
-    public function resize(string $filename, int $width, int $height) {
+    public function resize(string $filename, int $width, int $height)
+    {
+        $originDir = Storage::path('images') . '/origin/'; // путь к оригинальным изображениям
+        $thumbnailDir = Storage::path('images') . '/thumbnail/'; // путь к сжатым изображениям
+
+        $info = pathinfo($filename); // информацию о пути к файлу
+
+        if (isset($info['extension'])) {
+            $extensions = ['jpeg', 'png', 'jpg', 'gif']; // допустимые расширения файлов
+
+            if (in_array($info['extension'], $extensions)) {
+                $extension = $info['extension'];
+
+                $newImage = $info['filename'] . '-' . $width . 'x' . $height . '.' . $extension;
+            } else {
+                $filename = 'noimage.gif';
+                $newImage = 'noimage-' . $width . 'x' . $height . '.gif';
+            }
+        } else {
+            $filename = 'noimage.gif';
+            $newImage = 'noimage-' . $width . 'x' . $height . '.gif';
+        }
+
+        if (file_exists($thumbnailDir . $newImage) && is_file($thumbnailDir . $newImage)) {
+            return 'thumbnail/' . $newImage;
+        }
+
+        $oldImage = $filename;
+
+        if (!file_exists($thumbnailDir . $newImage) || (filemtime($originDir . $oldImage) > filemtime($thumbnailDir . $newImage))) {
+            $path = '';
+
+            $directories = explode('/', dirname(str_replace('../', '', $newImage)));
+
+            foreach ($directories as $directory) {
+                $path = $path . '/' . $directory;
+
+                if (!file_exists($thumbnailDir . $path)) {
+                    @mkdir($thumbnailDir . $path, 0777);
+                }
+            }
+
+            $thumbnail = Image::make($originDir . $filename);
+
+            $thumbnail->fit($width, $height);
+
+            $thumbnail->save($thumbnailDir . $newImage);
+        }
+
+        return 'thumbnail/' . $newImage;
+    }
+
+    /**
+     * Выдаёт строку с путём к изображению с нужным размером
+     *
+     * @param string $filename Название файла изображения
+     * @param string|int[] $size размер нужного изображения,
+     * строкой с ключом массива sizes из файла config/image.php
+     * или массивом [ширина, высота]
+     * @return string
+     */
+    public function getImage(string $filename, $size = 'medium'): string
+    {
+        /* Проверка, является ли изображение ссылкой */
         if (filter_var($filename, FILTER_VALIDATE_URL)) {
             return $filename;
         } else {
-            $originDir = Storage::path('images') . '/origin/';
-            $thumbnailDir = Storage::path('images') . '/thumbnail/';
-
-            if (stripos($filename, 'http') === false && !file_exists($originDir . $filename) || !is_file($originDir . $filename)) {
-                $filename = "noimage.gif";
+            if (is_array($size)) {
+                $filename = $this->resize($filename, $size[0], $size[1]);
+            } else {
+                $sizes = config('image.sizes');
+                $filename = $this->resize($filename, $sizes[$size][0], $sizes[$size][1]);
             }
 
-            $info = pathinfo($filename);
-
-            $extension = $info['extension'];
-
-            $oldImage = $filename;
-            $newImage = $info['filename'] . '-' . $width . 'x' . $height . '.' . $extension;
-
-            if (!file_exists($thumbnailDir . $newImage) || (filemtime($originDir . $oldImage) > filemtime($thumbnailDir . $newImage))) {
-                $path = '';
-
-                $directories = explode('/', dirname(str_replace('../', '', $newImage)));
-
-                foreach ($directories as $directory) {
-                    $path = $path . '/' . $directory;
-
-                    if (!file_exists($thumbnailDir . $path)) {
-                        @mkdir($thumbnailDir . $path, 0777);
-                    }
-                }
-
-                $thumbnail = Image::make($originDir . $filename);
-
-                $thumbnail->fit($width, $height);
-
-                $thumbnail->save($thumbnailDir . $newImage);
-            }
-
-            return $newImage;
+            return '/images/' . $filename;
         }
     }
 }
