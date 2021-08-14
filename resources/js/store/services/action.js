09350b0538d9ffe2afaf1a49ba57a801d154e704
cancerogen16 <don544@mail.ru>
2021-08-14 13:31:54 +0500

    format store/services

--- resources/js/store/services/action.js
+++ resources/js/store/services/action.js
@@ -1,23 +1,17 @@
-import { ADD_SERVICES, CLEAR_SERVICES, DELETE_SERVICES } from "./action-types";
+import {ADD_SERVICES, CLEAR_SERVICES, DELETE_SERVICES} from "./action-types";
 
 export const addServices = (services) => ({
-
     type: ADD_SERVICES,
     services
-    
 });
 
 export const delServices = (id) => ({
-
     type: DELETE_SERVICES,
     id
-
 });
 
 export const clearServices = () => ({
-
     type: CLEAR_SERVICES,
-
 });
 
 
--- resources/js/store/services/reducer.js
+++ resources/js/store/services/reducer.js
@@ -1,10 +1,8 @@
-import { ADD_SERVICES, CLEAR_SERVICES, DELETE_SERVICES } from "./action-types"
+import {ADD_SERVICES, CLEAR_SERVICES, DELETE_SERVICES} from "./action-types"
 
 const initialServicesState = []
 
     
-
-
 const addServices = (state, action) => {
     return [
         ...action.services
@@ -12,33 +10,26 @@ const addServices = (state, action) => {
 }
 
 const clearServices = () => {
-    return [
-        
-    ]
+    return []
 }
 
 const deleteService = (state, action) => {
-
     return state.filter(item => item.id !== action.id);
-    
 }
 
 export const servicesReducer = (state = initialServicesState, action) => {
-
-    switch(action.type){
-        case ADD_SERVICES:{
+    switch (action.type) {
+        case ADD_SERVICES: {
             return addServices(state, action);
         }
-        case CLEAR_SERVICES:{
+        case CLEAR_SERVICES: {
             return clearServices();
         }
-        case DELETE_SERVICES:{
-            
+        case DELETE_SERVICES: {
+
         }
-        default:{
+        default: {
             return state
         }
-
     }
-
 }
