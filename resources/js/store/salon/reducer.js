467df5dac7e8560338b51aded0479b68312d9594
cancerogen16 <don544@mail.ru>
2021-08-14 13:29:23 +0500

    format store/salon

--- resources/js/store/salon/reducer.js
+++ resources/js/store/salon/reducer.js
@@ -1,60 +1,47 @@
-import { ADD_SALON, CLEAR_SALON, CREATE_SALON } from "./action-types"
+import {ADD_SALON, CLEAR_SALON, CREATE_SALON} from "./action-types"
 
-const initialSalonState = {
-    
-}
+const initialSalonState = {}
 
 const addSalon = (action) => {
-    
     return {
         ...action
     }
-
 }
 
 const createSalon = (action) => {
-    
     return {
         ...action
     }
-
 }
 
 const clearSalon = () => {
-    
     return {
         user_id: '',
-        title: '', 
+        title: '',
         slug: '',
-        main_photo: '', 
-        city: '', 
-        address: '', 
-        phone: '', 
-        description: '', 
+        main_photo: '',
+        city: '',
+        address: '',
+        phone: '',
+        description: '',
         rating: '',
         worktime: ''
     }
-
 }
 
-
-
 export const salonReducer = (state = initialSalonState, action) => {
-
-    switch(action.type){
-        case ADD_SALON:{
+    switch (action.type) {
+        case ADD_SALON: {
             return addSalon(action)
         }
-        case CREATE_SALON:{
+        case CREATE_SALON: {
             return createSalon(action)
         }
-        case CLEAR_SALON:{
+        case CLEAR_SALON: {
             return clearSalon();
         }
-        default:{
+        default: {
             return state
         }
-
     }
-
 }
