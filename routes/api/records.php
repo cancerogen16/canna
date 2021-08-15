5db5f7420a6bb84485d6cb560b383581c122663a
ruslan-kiiamov <xkijam@gmail.com>
2021-08-15 17:14:37 +0300

    recordController refactoring

--- routes/api/records.php
+++ routes/api/records.php
@@ -8,7 +8,7 @@ Route::group([
 ], function () {
     Route::get('/', [RecordController::class, 'index'])->name('records.index');
     Route::get('/{id}', [RecordController::class, 'show'])->name('records.show');
-    Route::get('/{id}/calendars', [RecordController::class, 'getCalendars'])->name('records.calendars');
+    Route::get('/{id}/calendars', [RecordController::class, 'сalendars'])->name('records.calendars');
     Route::post('/', [RecordController::class, 'store'])->name('records.store');
     Route::match(['put', 'patch'], '/{id}', [RecordController::class, 'update'])->name('records.update');
     Route::delete('/{id}', [RecordController::class, 'delete'])->name('records.delete');
