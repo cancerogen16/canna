751064977256cc1488085429ff5286e0eead52b1
cancerogen16 <don544@mail.ru>
2021-08-20 15:54:03 +0500

    format resources/js/pages/dashboard/views/masters.js

--- resources/js/pages/dashboard/views/masters.js
+++ resources/js/pages/dashboard/views/masters.js
@@ -1,16 +1,13 @@
-import { Button, Divider, List, ListItem } from '@material-ui/core'
-import { SettingsInputAntennaTwoTone, SystemUpdateOutlined } from '@material-ui/icons'
-import React, { useRef, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import {Button, List, ListItem} from '@material-ui/core'
+import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import FormMaster from '../../../components/Dashboard/FormMaster'
 import useFormMaster from '../../../components/Dashboard/FormMaster/useFormMaster'
 import Modal from '../../../components/Dialogs/Modal'
 import MasterPreview from '../../../components/Public/MasterPreview'
 import priviewMaster from '../../../components/Public/MasterPreview/style'
-import { delMaster } from '../../../store/master/action'
-import { fetchDeleteMaster } from '../../../store/master/thunks'
+import {fetchDeleteMaster} from '../../../store/master/thunks'
 import styleMasters from '../styles/masters'
-
 
 export default function Page(props) {
     const formMaster = useFormMaster();
@@ -19,30 +16,28 @@ export default function Page(props) {
     const classes = priviewMaster();
     const masters = useSelector(state => state.masters);
     const pageStyle = styleMasters();
-    
-    
 
-    return  <div className={pageStyle.root}>
-                <Button className={pageStyle.addBtn} onClick={closeModal}>Добавить</Button>
-                <List>
-                {masters.map(master =>{
-                    return  <ListItem key={master.id} className={classes.root}  button>
-                        <MasterPreview   master={master}>
-                            <Button onClick={() => openModal(master, () => setUpdate(true))}>Редактировать</Button>
-                            <Button onClick={() => dispatch(fetchDeleteMaster(master.id))}>Удалить</Button>
-                        </MasterPreview>
-                            </ListItem>              
-                })}
-            </List>
-            <Modal 
-                open={open}
-                title="Укажите информацию о сотруднике"
-            >
-                <FormMaster
+    return <div className={pageStyle.root}>
+        <Button className={pageStyle.addBtn} onClick={closeModal}>Добавить</Button>
+        <List>
+            {masters.map(master => {
+                return <ListItem key={master.id} className={classes.root} button>
+                    <MasterPreview master={master}>
+                        <Button onClick={() => openModal(master, () => setUpdate(true))}>Редактировать</Button>
+                        <Button onClick={() => dispatch(fetchDeleteMaster(master.id))}>Удалить</Button>
+                    </MasterPreview>
+                </ListItem>
+            })}
+        </List>
+        <Modal
+            open={open}
+            title="Укажите информацию о сотруднике"
+        >
+            <FormMaster
                 useHuck={formMaster}
-                    submit={(e) => handleSubmit(e, closeModal)} 
-                    close={closeModal} 
-                />
-            </Modal>
-            </div>
+                submit={(e) => handleSubmit(e, closeModal)}
+                close={closeModal}
+            />
+        </Modal>
+    </div>
 }
