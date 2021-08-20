2b0ecf663869a48e37e15f382e1dc441a579d8c0
cancerogen16 <don544@mail.ru>
2021-08-20 15:54:52 +0500

    format resources/js/components/Dashboard/FormMaster/index.js

--- resources/js/components/Dashboard/FormMaster/index.js
+++ resources/js/components/Dashboard/FormMaster/index.js
@@ -1,10 +1,9 @@
-import React, { useRef } from 'react'
-import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator'
-import { Dialog, Button, DialogContent, DialogContentText, DialogTitle, DialogActions, TextField } from '@material-ui/core'
+import React from 'react'
+import {TextValidator, ValidatorForm} from 'react-material-ui-form-validator'
+import {Button, TextField} from '@material-ui/core'
 import formMaster from './style';
-import useFormMaster from './useFormMaster'
 
-export default function FormMaster (props){
+export default function FormMaster(props) {
     const classes = formMaster();
     const {
         handlerOnChangeField,
@@ -13,69 +12,67 @@ export default function FormMaster (props){
         credentials,
         open,
     } = props.useHuck
-    return  <ValidatorForm
-                className={classes.root}
-                onSubmit={props.submit}
-            >
-                <TextValidator
-                    className={classes.item}
-                    label="Имя"
-                    onChange={handlerOnChangeField}
-                    name="name"
-                    value={credentials.name}
-                    validators={['required']}
-                    errorMessages={['Поле обязательно для заполнения']}
-                />
-                <TextValidator
-                    className={classes.item}
-                    label="Должность"
-                    onChange={handlerOnChangeField}
-                    name="position"
-                    value={credentials.position}
-                    validators={['required']}
-                    errorMessages={['Поле обязательно для заполнения']}
-                />
-                <TextValidator
-                    className={classes.item}
-                    label="Опыт работы"
-                    onChange={handlerOnChangeField}
-                    name="experience"
-                    value={credentials.experience}
-                    validators={['required']}
-                    errorMessages={['Поле обязательно для заполнения']}
-                />
-
-                <input
-                    type="file"
-                    name='photo'
-                    onChange={handlerOnChangeField}
-                />
-                <TextField
-                    className={classes.areal}
-                    label="Описание"
-                    name='description'
-                    multiline
-                    maxRows={4}
-                    value={credentials.description}
-                    onChange={handlerOnChangeField}
-                />
-                <br/>
-
-                <Button
-                    className={classes.item}
-                    color="primary"
-                    variant="contained"
-                    type="submit"
-                >
-                    Сохранить
-                </Button>
-                <Button
-                    onClick={props.close}
-                    className={classes.item}
-                    color="secondary"
-                    variant="contained"
-                >
-                    Отмена
-                </Button>
-            </ValidatorForm>
+    return <ValidatorForm
+        className={classes.root}
+        onSubmit={props.submit}
+    >
+        <TextValidator
+            className={classes.item}
+            label="Имя"
+            onChange={handlerOnChangeField}
+            name="name"
+            value={credentials.name}
+            validators={['required']}
+            errorMessages={['Поле обязательно для заполнения']}
+        />
+        <TextValidator
+            className={classes.item}
+            label="Должность"
+            onChange={handlerOnChangeField}
+            name="position"
+            value={credentials.position}
+            validators={['required']}
+            errorMessages={['Поле обязательно для заполнения']}
+        />
+        <TextValidator
+            className={classes.item}
+            label="Опыт работы"
+            onChange={handlerOnChangeField}
+            name="experience"
+            value={credentials.experience}
+            validators={['required']}
+            errorMessages={['Поле обязательно для заполнения']}
+        />
+        <input
+            type="file"
+            name='photo'
+            onChange={handlerOnChangeField}
+        />
+        <TextField
+            className={classes.areal}
+            label="Описание"
+            name='description'
+            multiline
+            maxRows={4}
+            value={credentials.description}
+            onChange={handlerOnChangeField}
+        />
+        <br/>
+        <Button
+            className={classes.item}
+            color="primary"
+            variant="contained"
+            type="submit"
+        >
+            Сохранить
+        </Button>
+        <Button
+            onClick={props.close}
+            className={classes.item}
+            color="secondary"
+            variant="contained"
+        >
+            Отмена
+        </Button>
+    </ValidatorForm>
 }
