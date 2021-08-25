a8742fdc60b2e2a64e4470d2a5313e146bdcc253
Legotin A.V <ant.legotin@yandex.ru>
2021-08-25 21:39:35 +0500

    Добален функционал записи на услугу

--- resources/js/store/times/thunks.js
+++ resources/js/store/times/thunks.js
@@ -17,3 +17,37 @@ export const fetchTimesMaster = (master_id, date = '') => (dispatch, getState) =
             dispatch(addError({code: status, message: 'Что-то пошло не так'})) 
         }})
 }
+
+export const fetchTimesUpdate = (id, record_id) => (dispatch, getState) => {
+    HTTP.get(`/api/calendars/${id}`)
+    .then(res => {
+        console.log(res);
+        //dispatch(addTimeAll(res.data.calendars));
+    })
+    .catch(err => { 
+        if (err.response) { 
+            dispatch(addError({code: status, message: err.response.data.message})) 
+        } else if (err.request) { 
+            dispatch(addError({code: status, message: 'Не удается соединится с сервером'}))
+        } else { 
+            dispatch(addError({code: status, message: 'Что-то пошло не так'})) 
+        }})
+}
+
+export const fetchRecorClear = (time, record) => (dispatch, getState) => {
+    HTTP.post(`/api/records`, record)
+    .then(res => {
+        console.log(res);
+        dispatch(fetchTimesUpdate(time.id, res.data.record.id))
+        //dispatch(addTimeAll(res.data.calendars));
+    })
+    .catch(err => { 
+        if (err.response) { 
+            dispatch(addError({code: status, message: err.response.data.message})) 
+        } else if (err.request) { 
+            dispatch(addError({code: status, message: 'Не удается соединится с сервером'}))
+        } else { 
+            dispatch(addError({code: status, message: 'Что-то пошло не так'})) 
+        }})
+}
+
